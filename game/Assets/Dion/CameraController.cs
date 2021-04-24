fc25bf7c07a4c64cbbe14bff9b723719203558fb
Guilherme Oliveira <htoguilherme@gmail.com>
2021-04-24 18:37:38 +1200

    Add camera controller

---
+++ game/Assets/Dion/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Dion;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] [Range(0, 1f)] private float _smoothnes;
+
+    private GameObject _player;
+    private Vector3 _initialPosition;
+    
+    void Start()
+    {
+        _player = FindObjectOfType<DionController>().gameObject;
+        _initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var target = new Vector3(_initialPosition.x, _player.transform.position.y, _initialPosition.z);
+        transform.position = Vector3.Lerp(transform.position, target, _smoothnes * Time.deltaTime);
+    }
+}
