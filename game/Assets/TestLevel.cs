f77c689de4b9a0e4b8a1da3c1fd752e392d33dba
Philip <philip.d.plant@gmail.com>
2021-04-26 21:01:04 +1200

    Add blue lighting and new prefabs

--- game/Assets/TestLevel.cs
+++ game/Assets/TestLevel.cs
@@ -16,45 +16,35 @@ public class TestLevel : MonoBehaviour
         Level level = new Level();
 
         //Wire first tile
-        var firstTile = new LevelTile(1,1,level);
+        var firstTile = new LevelTile(3,3,level);
         InstantiateTile(tilePrefabs[0], new LevelGridVector2(0, 0), firstTile, level);
-        firstTile.AddEntrance(0,1,EDirection.South);
+        firstTile.AddEntrance(1,3,EDirection.South);
         
         //### Wire up the straight 1 by 1 tile ###
-        var straightTile = CreateTemplate(tilePrefabs[0], 1, 1, level);
-        straightTile.AddEntrance(0,0,EDirection.North);
-        straightTile.AddEntrance(0,1,EDirection.South);
+        var straightTile = CreateTemplate(tilePrefabs[0], 3, 3, level);
+        straightTile.AddEntrance(1,0,EDirection.North);
+        straightTile.AddEntrance(1,3,EDirection.South);
         level.candidateTiles.Add(straightTile);
 
-        //### Wire up the side 1 by 1 tile ###
-        var sideTile1By1 = CreateTemplate(tilePrefabs[2], 1, 1, level);
-        sideTile1By1.AddEntrance(0,0,EDirection.West);
-        sideTile1By1.AddEntrance(1,0,EDirection.East);
-        level.candidateTiles.Add(sideTile1By1);
+        //### Wire up the bend 4 by 3 tile ###
+        var bend4X3 = CreateTemplate(tilePrefabs[1], 3, 3, level);
+        bend4X3.AddEntrance(1,0,EDirection.North);
+        bend4X3.AddEntrance(1,3,EDirection.South);
+        bend4X3.AddEntrance(3,1,EDirection.East);
+        level.candidateTiles.Add(bend4X3);
 
-        //### Wire up the two by two arena ###
-        var twoByTwoArena = CreateTemplate(tilePrefabs[1], 2, 2, level);
-        twoByTwoArena.AddEntrance(0,0,EDirection.North);
-        twoByTwoArena.AddEntrance(1,2,EDirection.South);
-        level.candidateTiles.Add(twoByTwoArena);
+        /*//### Wire up the bend 3 by 3 tile ###
+        var bend3X3 = CreateTemplate(tilePrefabs[2], 3, 3, level);
+        bend3X3.AddEntrance(1,0,EDirection.North);
+        bend3X3.AddEntrance(1,3,EDirection.South);
+        bend3X3.AddEntrance(0,1,EDirection.West);
+        level.candidateTiles.Add(bend3X3);*/
         
-        //### Wire up the two by two arena left side entrance ###
-        var twoByTwoArenaLeftSideEntrance = CreateTemplate(tilePrefabs[3], 2, 2, level);
-        twoByTwoArenaLeftSideEntrance.AddEntrance(1,0,EDirection.North);
-        twoByTwoArenaLeftSideEntrance.AddEntrance(2,0,EDirection.East);
-        level.candidateTiles.Add(twoByTwoArenaLeftSideEntrance);
-        
-        //### Wire up the two by two arena right side entrance ###
-        var twoByTwoArenaRightSideEntrance = CreateTemplate(tilePrefabs[4], 2, 2, level);
-        twoByTwoArenaRightSideEntrance.AddEntrance(1,0,EDirection.North);
-        twoByTwoArenaRightSideEntrance.AddEntrance(0,0,EDirection.West);
-        level.candidateTiles.Add(twoByTwoArenaRightSideEntrance);
-        
-        //### Wire up the two by two arena right side entrance ###
-        var twoByTwoArenaRightSideEntranceBottom = CreateTemplate(tilePrefabs[5], 2, 2, level);
-        twoByTwoArenaRightSideEntranceBottom.AddEntrance(0,0,EDirection.West);
-        twoByTwoArenaRightSideEntranceBottom.AddEntrance(1,2,EDirection.South);
-        level.candidateTiles.Add(twoByTwoArenaRightSideEntranceBottom);
+        //### Wire up the side straight tile ###
+        var sideStraight = CreateTemplate(tilePrefabs[3], 3, 3, level);
+        sideStraight.AddEntrance(3,1,EDirection.East);
+        sideStraight.AddEntrance(0,1,EDirection.West);
+        level.candidateTiles.Add(sideStraight);
         
         //Lastly generate some neighbours for the first tile so that you can't see (as much) pop in
         firstTile.GenerateNeighbours();
